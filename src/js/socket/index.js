9e2c391192fb25f747a6cbad8959493b6a1b9e48
Brett Cizmar <brett.cizmar@hotmail.com>
2018-08-18 23:24:54 -0700

    testing something.

--- src/js/socket/index.js
+++ src/js/socket/index.js
@@ -1,4 +1,4 @@
 import socketIOClient from "socket.io-client";
 
-const socketIOEndpoint = "http://0.0.0.0:4001";
+const socketIOEndpoint = "http://159.89.158.252:4001";
 export const socketClient = socketIOClient(socketIOEndpoint);
